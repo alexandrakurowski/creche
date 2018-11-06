8eb651749d44f1625c7cba3fdfdabc8993997ead
alexandrakurowski <alexandrakuro@gmail.com>
2018-11-06 10:21:08 +0100

    suite du projet

--- child_details.php
+++ child_details.php
@@ -1,1 +1,58 @@
-child_details.php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<title>LogiCrèche</title>
+
+</head>
+<body>
+
+<?php
+
+require ('connect.php')
+
+?>
+	<h4>détails de l'enfant</h4>
+<?php
+if(isset($_POST['child_details'])){
+var_dump($child_details);
+
+ $details = $db->query('SELECT * FROM children WHERE children_id = '.$list['children_id'].'');
+
+
+	$children_lastname = $_POST ['children_lastname'];
+  $children_firstname = $_POST ['children_firstname'];
+  $children_birthday = $_POST['children_birthday'];
+  $children_adress = $_POST ['children_adress']; 
+  $children_parents_contact = $_POST ['children_parents_contact'];
+  $children_remarks = $_POST ['children_remarks'];
+
+	var_dump($children_lastname);
+}
+
+	          
+
+	//      while ($list = $children->fetch())
+	//     	{
+
+?> 
+$idChild = $_POST['children_id'];
+$child = $db->query("SELECT * FROM children WHERE children_id = " . $idChild);
+		while($datas = $child->fetch())
+		{
+			echo 
+				"<div>" . $datas['children_lastname'] . "</div>
+				<div>" . $datas['children_firstname'] . "</div>
+				<div>" . $datas['children_birthday'] . "</div>
+				<div>" . $datas['children_adress'] . "</div>
+				<div>" . $datas['children_parents_contact'] . "</div>
+				<div>" . $datas['children_remarks'] . "</div>";
+		}
+</body>
+</html>
--- index.php
+++ index.php
@@ -38,14 +38,19 @@ require ("connect.php");
 <?php
 
 	$children = $db->query('SELECT * FROM children');
-	            
+	          
 
 	     while ($list = $children->fetch())
 	    	{
+	    		$child_details = $list['children_id'];
 ?>
 			     <tr>
-
-			      <th scope="row"><?php echo $list['children_id']; ?><button class="view_child" name="view_child"  onclick="self.location.href='child_details.php'"><img class="icon bin" src="images/view.png"></button></th>
+					
+			      <th scope="row"><?php echo $list['children_id']; ?>
+			      <a href="child_details.php" class="idChild" name="children_id" value="' . $list['children_id'] . '">Voir détails</a>
+			      	
+			      </th>
+			 	
 			      <td><?php echo $list['children_firstname']; ?></td>
 			      <td><?php echo $list['children_lastname']; ?></td>
 			      <td>
@@ -54,32 +59,7 @@ require ("connect.php");
 					  <label class="custom-control-label" for="customRadio1">valider si enfant présent</label>
 <?php 
 //requete if radio select into tabla child care
-// <!-- Button trigger modal -->
-// <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-//   Launch demo modal
-// </button>
-
-// <!-- Modal -->
-// <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-//   <div class="modal-dialog" role="document">
-//     <div class="modal-content">
-//       <div class="modal-header">
-//         <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-//         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-//           <span aria-hidden="true">&times;</span>
-//         </button>
-//       </div>
-//       <div class="modal-body">
-//         ...
-//       </div>
-//       <div class="modal-footer">
-//         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-//         <button type="button" class="btn btn-primary">Save changes</button>
-//       </div>
-//     </div>
-//   </div>
-// </div>
-
+var_dump($child_details);
 ?>
 					</div>
 				</td>
@@ -96,6 +76,28 @@ $children->closeCursor(); // end and close the request children
 			
 		
 	</div>
+
+<!-- MODAL for child's details -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 	<div>
 		
